73eef71ff148a65763f82ed611f497b8a0811aac
IBR <u2024413@giki.edu.pk>
2026-07-14 16:33:47 +0500

    updated schema to use .email method instead of .regex

--- src/schemas/signUpSchema.ts
+++ src/schemas/signUpSchema.ts
@@ -2,6 +2,6 @@ import {z} from 'zod'
 
 export const signUpSchema=z.object({
     userName: z.string().min(3, 'Username must be more than 3 characters').max(30, "Username can't be more than 30 characters"),
-    email: z.string().regex(/^[a-z0-9@.]+$/, "Invalid email format"),
+    email: z.string().email(),
     password: z.string().min(3, 'Password must be more than 3 characters')
 })
